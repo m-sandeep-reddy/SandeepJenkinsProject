73bbb8b442d3092b93abfcf7cbad6e419d909e9e
SANDEEP <SANDEEP@192.168.1.28>
2025-03-12 19:59:46 +0530

    jenkins 1st push

---
+++ Jenkins/src/test/java/com/jenkins/testscript/Open_Browser.java
@@ -0,0 +1,24 @@
+package com.jenkins.testscript;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Open_Browser {
+	
+	@Test
+	public void demo()
+	{
+		Reporter.log("browser is created succesfulluy",true);
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.instagram.com/");
+		Reporter.log(driver.getTitle(),true);
+		driver.quit();
+	}
+
+}
